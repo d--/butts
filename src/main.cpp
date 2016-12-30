c70b5cab65ab12235220a66991539e08cd37837f
d-- <d--@users.noreply.github.com>
2016-12-29 19:07:12 -0500

    adding basic OpenGL - will need to be modified for OSX/Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,55 @@
 #include <iostream>
 #include "SDL.h"
 
+#ifdef _WIN32
+#include <GL/glew.h>
+#endif
+
 int main(int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Could not init SDL: %s\n", SDL_GetError());
         SDL_Quit();
         return 1;
     }
-    std::cout << "Hello, butts!" << std::endl;
+
+    Uint32 flags;
+    flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN;
+
+    SDL_Window *window;
+    window = SDL_CreateWindow(
+        "Butts",                    // title
+        SDL_WINDOWPOS_UNDEFINED,    // x
+        SDL_WINDOWPOS_UNDEFINED,    // y
+        400,                        // w
+        400,                        // h
+        flags                       // flags
+    );
+    
+    if (window == NULL) {
+        SDL_Log("Could not create window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_GLContext gl = SDL_GL_CreateContext(window);
+
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    SDL_GL_SwapWindow(window);
+
+    SDL_Event event;
+
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+    }
+
+    SDL_GL_DeleteContext(gl);
     SDL_Quit();
     return 0;
 }
